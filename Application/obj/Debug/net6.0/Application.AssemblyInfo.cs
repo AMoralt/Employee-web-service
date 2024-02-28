8e532b90e805bf3aac1e75df26ff8f3d9e74aa42
AMoralt <dias_galym@bk.ru>
2024-02-29 01:28:17 +0500

    Исправил название переменной в методе Delete в EmployeeController

--- Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8d98bab3d9419d6e1567b0548dd047609ae158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3e44d645661e15c8b05b1b4c989ba01331ebb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
